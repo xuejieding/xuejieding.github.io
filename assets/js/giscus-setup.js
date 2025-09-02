9c79906cbeeb15bb212ea28be4cb4dc01c3c62a9
xuejieding <33223348+xuejieding@users.noreply.github.com>
2025-09-02 08:36:46 +0000

    Deploying to gh-pages from @ xuejieding/xuejieding.github.io@7f4d556b04da56bf72f66b98e000d79eae8d28e9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "xuejieding/website",
+    "data-repo": "xuejieding/xuejieding.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
